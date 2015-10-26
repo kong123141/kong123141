4805648f64303cf0570ced412782c63a0477bdb7
晴依 <L#>
2015-10-26 16:05:11 +0800

    添加菜单背景色和透明度和选中菜单项的背景皱巴巴和透明度

--- 【零度汉化】基础功能/Menu.cs
+++ 【零度汉化】基础功能/Menu.cs
@@ -229,12 +229,14 @@ namespace LeagueSharp.Common
 
         public static Color BackgroundColor
         {
-            get { return Color.FromArgb(Menu.root.Item("BackgroundAlpha").GetValue<Slider>().Value, Color.Black); }
-        }
+			//get { return Color.FromArgb(Menu.root.Item("BackgroundAlpha").GetValue<Slider>().Value, Menu.root.Item("BackgroundColor").GetValue<Color>()); }
+			get { return Menu.root.Item("BackgroundColor").GetValue<Color>(); }
+		}
 
         public static Color ActiveBackgroundColor
         {
-            get { return Color.FromArgb(0, 37, 53); }
+            //get { return Color.FromArgb(0, 37, 53); }
+			get { return Menu.root.Item("BackgroundColor").GetValue<Color>(); }
         }
 
         private static void Game_OnWndProc(WndEventArgs args)
@@ -412,8 +414,8 @@ namespace LeagueSharp.Common
 
             DrawBox(new Vector2(position.X + item.Height - 28, position.Y + 1),
                 (int) Font.MeasureText(item.Tooltip).Width + 8, item.Height, MenuSettings.BackgroundColor, 1, Color.Black);
-
-            var s = item.Tooltip;
+			
+			var s = item.Tooltip;
             Font.DrawText(
                 null, s,
                 new Rectangle(
@@ -444,11 +446,13 @@ namespace LeagueSharp.Common
         {
 			root.AddItem(
 				new MenuItem("Language", "选择语言:").SetValue(
-					new StringList(new[] { "默认", "中文", "英语" }, 0)));
-            root.AddItem(new MenuItem("BackgroundAlpha", "Background Opacity")).SetValue(new Slider(165, 55, 255));
-            root.AddItem(
+					new StringList(new[] { "默认", "中文", "英语" }, 1)));
+			root.AddItem(new MenuItem("BackgroundColor", "背景颜色及透明度").SetValue(System.Drawing.Color.Black));
+			root.AddItem(new MenuItem("ActiveBackgroundColor", "选项颜色及透明度").SetValue(System.Drawing.Color.FromArgb(0, 37, 53)));
+			//root.AddItem(new MenuItem("BackgroundAlpha", "Background Opacity")).SetValue(new Slider(165, 55, 255));
+			root.AddItem(
                 new MenuItem("FontName", "Font Name:").SetValue(
-                    new StringList(new[] { "Tahoma", "Calibri", "Segoe UI" }, 0)));
+                    new StringList(new[] { "Tahoma", "Calibri", "Segoe UI", "Microsoft YaHei" }, 0)));
             root.AddItem(new MenuItem("FontSize", "Font Size:").SetValue(new Slider(13, 12, 20)));
             var qualities = Enum.GetValues(typeof(FontQuality)).Cast<FontQuality>().Select(v => v.ToString()).ToArray();
             root.AddItem(new MenuItem("FontQuality", "Font Quality").SetValue(new StringList(qualities, 4)));
