4e00c3a6c9413cc387c7c0b80061758be981607c
晴依 <L#>
2015-10-26 16:07:31 +0800

    修正屏蔽显示，修复屏蔽脚本载入信息

--- 【零度汉化】基础功能/Flowers.cs
+++ 【零度汉化】基础功能/Flowers.cs
@@ -8,7 +8,7 @@
     {
         public static bool duramk = false;
         public static float gameTime1 = 0;
-        public static readonly Menu fl = new Menu("开发功能", "Flowers Utility");
+        public static readonly Menu fl = new Menu("实用功能", "Flowers Utility");
 
         internal static void Initilalize()
         {
@@ -17,27 +17,46 @@
 
         private static void Game_OnGameLoad(EventArgs args)
         {
-            fl.AddItem(new MenuItem("Disable Drawing", "屏蔽线圈开关 默认按键:Home").SetValue(new KeyBind(36, KeyBindType.Toggle)));
-            fl.AddItem(new MenuItem("zoom hack", "无限视距").SetValue(false));
-            fl.AddItem(new MenuItem("disable say", "禁止脚本发聊天信息").SetValue(true));
-            fl.AddItem(new MenuItem("Tower Ranges", "显示防御塔范围(靠近)").SetValue(false));
-            fl.AddItem(new MenuItem("SaySomething", "开局自动屏蔽掉聊天框信息").SetValue(false));
+            fl.AddItem(new MenuItem("Disable Drawing", "屏蔽显示[Home]").SetValue(new KeyBind(36, KeyBindType.Toggle, LeagueSharp.Hacks.DisableDrawings)));
+            fl.AddItem(new MenuItem("zoom hack", "无限视距[危险]").SetValue(false)); 
+            fl.AddItem(new MenuItem("disable say", "禁止脚本发话").SetValue(true));
+            fl.AddItem(new MenuItem("Tower Ranges", "显示敌方防御塔范围").SetValue(false));
+            fl.AddItem(new MenuItem("SaySomething", "刷屏阻止脚本载入信息").SetValue(false));
             fl.AddItem(new MenuItem("SaySomething1", "By 花边"));
             CommonMenu.Config.AddSubMenu(fl);
 
-            if (fl.Item("SaySomething").IsActive())
+			fl.Item("Disable Drawing").ValueChanged += Flowers_ValueChanged;
+
+			if (fl.Item("SaySomething").GetValue<bool>())
             {
-                Thread.Sleep(1000);
-                Game.Say("/");
-                Game.Say("/");
-                Game.Say("/");
-                Game.Say("/");
+				Utility.DelayAction.Add(1000,() => {
+					Game.PrintChat("銆€"); 
+					Game.PrintChat("銆€");
+					Game.PrintChat("銆€");
+					Game.PrintChat("銆€");
+					Game.PrintChat("銆€");
+					Game.PrintChat("銆€");
+					Game.PrintChat("銆€");
+					Game.PrintChat("銆€");
+					Game.PrintChat("<font color=\"#FFA042\"><b>杈撳嚭/ help鑾峰彇鍛戒护鍒楄〃</b></font>");
+				});
             }
 
             Game.OnUpdate += Game_OnUpdate;
         }
 
-        private static void Game_OnUpdate(EventArgs args)
+		private static void Flowers_ValueChanged(object sender, OnValueChangeEventArgs e) {
+			if (fl.Item("Disable Drawing").GetValue<KeyBind>().Active)
+			{
+				LeagueSharp.Hacks.DisableDrawings = true;
+            }
+			else
+			{
+				LeagueSharp.Hacks.DisableDrawings = false;
+			}
+		}
+
+		private static void Game_OnUpdate(EventArgs args)
         {
             if (fl.Item("zoom hack").IsActive())
             {
@@ -46,15 +65,6 @@
             else
             {
                 Lost.ZoomHack = false;
-            }
-
-            if (fl.Item("Disable Drawing").GetValue<KeyBind>().Active)
-            {
-                Lost.DisableDrawings = true;
-            }
-            else
-            {
-                Lost.DisableDrawings = false;
             }
 
             if (fl.Item("disable say").GetValue<KeyBind>().Active)
